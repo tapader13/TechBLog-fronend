455c90e2f329de4293084903906da9d49fe92e0a
tapader13 <minhajtapader0@gmail.com>
2025-01-07 22:57:38 -0500

    firebase initialize

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,16 @@
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 import ScrollEffect from '@/components/ScrollEffect';
+import AuthContext from '@/context/AuthContext';
 import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <AuthContext>
       <ScrollEffect />
       <Header />
       <Component {...pageProps} />
       <Footer />
-    </>
+    </AuthContext>
   );
 }
