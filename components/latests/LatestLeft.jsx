660881e559430fcfbf17f1e1e9ac805f09445afa
tapader13 <minhajtapader0@gmail.com>
2025-01-11 00:37:28 -0500

    changes i url

--- components/latests/LatestLeft.jsx
+++ components/latests/LatestLeft.jsx
@@ -13,7 +13,7 @@ const LatestLeft = () => {
     const getLatestPosts = async () => {
       setIsLoading(true);
       const res = await fetch(
-        `http://localhost:3000/api/blogapi?specificCategory=latest`
+        `https://blog-frnt.vercel.app/api/blogapi?specificCategory=latest`
       );
       if (!res.ok) {
         throw new Error(`Error: ${res.status} ${res.statusText}`);
